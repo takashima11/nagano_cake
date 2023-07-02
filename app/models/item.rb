36b40a38b883e819b54062111e29b7bca709703a
takashima11 <129072192+takashima11@users.noreply.github.com>
2023-07-02 07:07:19 +0000

    Add order

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image
+    image.variant(resize_to_limit: [width, heigt]).processed
   end
 
   def with_tax_price
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,18 @@ class Order < ApplicationRecord
   enum payment_method: { credit_card: 0, transfer: 1 }
   belongs_to :customer
   has_many :order_details
+
+  # def address_display
+  #   '〒' + postal_code + '  ' + address + '  ' + name
+  # end
+
+  def item_quantity
+    quantity =0
+    order_details.each do |order_detail|
+      quantity = quantity + order_detail.quantity
+    end
+    return quantity
+  end
+
+
 end
